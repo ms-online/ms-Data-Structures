404888dc6315edf0527a9f249a87c6145580a399
ms-online <27732357@qq.com>
2021-01-07 20:00:04 +0800

    lesson-2

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-const strings = ['x', 'a', 'y', 'b', 'c', 'd']
-//    0    1    2    3    4   5
-// 4*4 =16 存储的字节
+const strings = ['a', 'b', 'c', 'd']
+// //    0    1    2    3    4   5
+// // 4*4 =16 存储的字节
 
-// console.log(strings[2]) //O(1)
+// // console.log(strings[2]) //O(1)
 
-//push
-strings.push('e') //O(1)
+// //push
+// strings.push('e') //O(1) O(n)
 
-//pop
-strings.pop()
-strings.pop() //O(1)
+// //pop
+// strings.pop()
+// strings.pop() //O(1)
 
-//unshift
-strings.unshift('x') //O(n)
+// //unshift
+// strings.unshift('x') //O(n)
 
-//splice
-strings.splice(2, 0, 'y') //O(n)
-console.log(strings)
+// //splice
+// strings.splice(2, 0, 'y') //O(n)
+// console.log(strings)
+
+// C++
+// int a[20];
+// int b[5] {1, 2, 3, 4, 5}
