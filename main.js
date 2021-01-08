3a6b268da73b0b8f8c61e01b348b6aeef7c2940c
ms-online <27732357@qq.com>
2021-01-08 18:13:42 +0800

    lesson-11

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const basket = ['apples', 'grapes', 'pears']
+// const basket = ['apples', 'grapes', 'pears']
 
 // 链表：apples --> grapes --> pears
 
@@ -6,3 +6,12 @@ const basket = ['apples', 'grapes', 'pears']
 // 8947 --> grapes
 //           8742 --> pears
 //                     372 --> null
+
+
+//指针
+const obj1 = { a: true }
+const obj2 = obj1
+obj1.a = 'hello'
+delete obj1
+// console.log('1', obj1)
+console.log('2', obj2)
